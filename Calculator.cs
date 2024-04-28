869c5b3f314899e789b23758da76341aa526daa3
SashveerRamjathan <sashveerlakhan0@gmail.com>
2024-04-28 17:28:33 +0200

    Added comments to code

--- Calculator.cs
+++ Calculator.cs
@@ -8,35 +8,46 @@ namespace ST10361554_PROG6221_ICE_Task_4
 {
     public class Calculator
     {
+        // A property for the first number in a calculation
         public double Num1 { get; set; }
+
+        // A property for the second number in a calculation
         public double Num2 { get; set; }
+
+        // A property for the result of a calculation
         public double Result { get; set; }
 
+        // A delegate that represents an event raised when a calculation is performed
         public delegate void CalculationPerformedEventHandler(string operationPerformed);
 
+        // An event that is raised when a calculation is performed
         public event CalculationPerformedEventHandler? CalculationPerformed;
 
+        // A method that performs addition on two numbers and raises the CalculationPerformed event
         public void Add(double num1, double num2)
         {
             this.Result = num1 + num2;
             OnCalculationPerformed("Addition");
         }
 
+        // A method that performs subtraction on two numbers and raises the CalculationPerformed event
         public void Subtract(double num1, double num2)
         {
             this.Result = num1 - num2;
             OnCalculationPerformed("Subtraction");
         }
 
+        // A method that performs multiplication on two numbers and raises the CalculationPerformed event
         public void Multiply(double num1, double num2)
         {
             this.Result = num1 * num2;
             OnCalculationPerformed("Multiplication");
         }
 
+        // A method that performs division on two numbers and raises the CalculationPerformed event
         public void Divide(double num1, double num2)
         {
-            if (num2 != 0)
+            if (num2!= 0)
             {
                 this.Result = num1 / num2;
                 OnCalculationPerformed("Division");
@@ -44,9 +55,10 @@ namespace ST10361554_PROG6221_ICE_Task_4
             else throw new Exception(message: "Can't Divide by 0");
         }
 
+        // A method that raises the CalculationPerformed event
         public virtual void OnCalculationPerformed(string operationPerformed)
         {
-            if (CalculationPerformed != null)
+            if (CalculationPerformed!= null)
             {
                 CalculationPerformed(operationPerformed);
             }
--- CalculatorForm.cs
+++ CalculatorForm.cs
@@ -2,83 +2,112 @@ namespace ST10361554_PROG6221_ICE_Task_4
 {
     public partial class CalculatorForm : Form
     {
+        // Default constructor for the CalculatorForm class
         public CalculatorForm()
         {
+            // Initialize the components of the form
             InitializeComponent();
         }
 
+        // Event handler for the calculateButton click event
         private void calculateButton_Click(object sender, EventArgs e)
         {
             try
             {
+                // Validate input fields before performing any calculations
                 if (ValidateInput())
                 {
+                    // Instantiate a new Calculator object
                     Calculator calc = new Calculator();
 
+                    // Get the first number from the firstNumberTextbox and convert it to a double
                     string firstNumber = firstNumberTextbox.Text;
                     double num1 = Convert.ToDouble(firstNumber);
                     calc.Num1 = num1;
 
+                    // Get the second number from the secondNumberTextbox and convert it to a double
                     string secondNumber = secondNumberTextbox.Text;
                     double num2 = Convert.ToDouble(secondNumber);
                     calc.Num2 = num2;
 
+                    // Get the selected operation from the operationComboBox
                     string operation = operationComboBox.Text;
 
+                    // Perform the selected operation using the Calculator object
                     if (operation.Equals("Addition"))
                     {
+                        // Subscribe to the CalculationPerformed event and display a success message with the result
                         calc.CalculationPerformed += (operation) => MessageBox.Show($"Operation performed: {operation} \n Result: {calc.Result}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
                         calc.Add(num1, num2);
                     }
                     else if (operation.Equals("Subtraction"))
                     {
+                        // Subscribe to the CalculationPerformed event and display a success message with the result
                         calc.CalculationPerformed += (operation) => MessageBox.Show($"Operation performed: {operation} \n Result: {calc.Result}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
                         calc.Subtract(num1, num2);
                     }
                     else if (operation.Equals("Multiplication"))
                     {
+                        // Subscribe to the CalculationPerformed event and display a success message with the result
                         calc.CalculationPerformed += (operation) => MessageBox.Show($"Operation performed: {operation} \n Result: {calc.Result}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
                         calc.Multiply(num1, num2);
                     }
                     else if (operation.Equals("Division"))
                     {
+                        // Subscribe to the CalculationPerformed event and display a success message with the result
                         calc.CalculationPerformed += (operation) => MessageBox.Show($"Operation performed: {operation} \n Result: {calc.Result}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
                         calc.Divide(num1, num2);
                     }
                 }
-                else throw new Exception(message: "Check input values before continuing");
+                else
+                {
+                    // Throw an exception if the input is not valid
+                    throw new Exception(message: "Check input values before continuing");
+                }
             }
             catch (Exception ex)
             {
+                // Display an error message if an exception is thrown
                 MessageBox.Show($"An Error has occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             finally
             {
+                // Clear the input fields and reset the operationComboBox after the calculation is performed
                 firstNumberTextbox.Clear();
                 secondNumberTextbox.Clear();
                 operationComboBox.SelectedIndex = -1;
             }
         }
 
+        // This method, ValidateInput, checks if the input fields in the user interface are valid
         public bool ValidateInput()
         {
+            // Initialize a boolean variable, isValid, to true
             bool isValid = true;
 
+            // Check if the text box for the first number is null or empty
             if (firstNumberTextbox.Text == null || firstNumberTextbox.Text.Equals(""))
             {
+                // If it is, set isValid to false
                 isValid = false;
             }
 
+            // Check if the text box for the second number is null or empty
             if (secondNumberTextbox.Text == null || secondNumberTextbox.Text.Equals(""))
             {
+                // If it is, set isValid to false
                 isValid = false;
             }
 
+            // Check if the selected index of the combo box for the operation is -1
+            // (which means no operation is selected)
             if (operationComboBox.SelectedIndex == -1)
             {
+                // If it is, set isValid to false
                 isValid = false;
             }
 
+            // Return the value of isValid
             return isValid;
         }
     }
